57f976103be38ea2da63481ab0a631d3f35d6869
foxcrate <ahmedmustafa.pro19@gmail.com>
2021-11-04 09:34:56 -0700

    everything ihs fine in my home

--- courses/app/Imports/ExcelImport.php
+++ courses/app/Imports/ExcelImport.php
@@ -5,24 +5,25 @@ namespace App\Imports;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\ToModel;
-// use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
 
 // , withHeadingRow
-class ExcelImport implements ToCollection
+class ExcelImport implements ToCollection, WithHeadingRow
 {
     /**
     * @param Collection $collection
     */
     public function collection(Collection $collection)
     {
-        foreach($collection as $row){
+        foreach($collection as $key=>$row){
             
             $u1 = User::create([
-                'name'=>$row[0],
-                'email'=>$row[1],
+                'id'=>$key+40,
+                'name'=>$row['name'],
+                'email'=>$row['email'],
                 'password'=>hash::make("12345"),
             ]);
 
